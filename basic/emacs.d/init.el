18a65e3cbd0b850954e5bd5e6403747b92ce054f
Marshall T. Vandegrift <llasram@gmail.com>
2012-12-07 02:59:25 -0500

    Yet another Ruby DSL file.

--- basic/emacs.d/init.el
+++ basic/emacs.d/init.el
@@ -99,6 +99,7 @@
 
 (add-to-list 'auto-mode-alist '("\\(?:^\\|/\\)Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\(?:^\\|/\\)Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\(?:^\\|/\\)Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
