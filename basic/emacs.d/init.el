fa75db9cff5fb1e9b8894fb269c8e049608dbb5c
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2012-12-20 15:51:38 -0500

    More rake files for ruby-mode.

--- basic/emacs.d/init.el
+++ basic/emacs.d/init.el
@@ -101,6 +101,7 @@
 (add-to-list 'auto-mode-alist '("\\(?:^\\|/\\)Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\(?:^\\|/\\)Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
