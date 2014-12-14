1f9944b0e2f1b86a4569d0192ce7e1eb7ff19255
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2014-12-14 11:35:02 -0500

    Update to most new lein-hadoop-cluster release.

--- basic/lein/profiles.clj
+++ basic/lein/profiles.clj
@@ -3,7 +3,7 @@
             [lein-ancient "0.5.5"]
             [lein-difftest "2.0.0"]
             [org.clojars.briprowe/lein-s3-repo "0.1.1"]
-            [lein-hadoop-cluster "0.1.3"]
+            [lein-hadoop-cluster "0.1.4"]
             [cider/cider-nrepl "0.8.1"]]
   :dependencies [[alembic "0.2.1"]
                  [criterium "0.4.3"]]}}
