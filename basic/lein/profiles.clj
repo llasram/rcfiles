8de5b6bf2013d84dec27c5d76c3d35b568320569
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2016-02-15 05:57:25 -0500

    Latest CIDER configuration

--- basic/lein/profiles.clj
+++ basic/lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user
  {:plugins [[lein-pprint "1.1.1"]
-            [lein-ancient "0.6.8"]
-            [cider/cider-nrepl "0.10.1"]]
-  :dependencies [[alembic "0.3.2"]
+            [lein-ancient "0.6.8"]]}
+ :repl
+ {:plugins [[cider/cider-nrepl "0.10.2"]]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                 [alembic "0.3.2"]
                  [criterium "0.4.3"]]}}
