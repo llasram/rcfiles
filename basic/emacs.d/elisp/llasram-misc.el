7b023345de6f934b46915fb1a989541262cb5b4d
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2017-04-06 09:39:46 -0400

    Function for shifting up named CSS font sizes

--- basic/emacs.d/elisp/llasram-misc.el
+++ basic/emacs.d/elisp/llasram-misc.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+
 (defun my/preserve-selected-window (f &rest args)
   "Function version of `save-selected-window'.
 Argument F is a function to invoke and optional ARGS any
@@ -17,6 +19,22 @@ arguments to `apply' that function to."
     (if sym
       (describe-symbol sym)
       (command-execute 'describe-symbol))))
+
+(defconst my/css-font-sizes
+  '("xx-large" "x-large" "large" "medium" "small" "x-small" "xx-small")
+  "CSS named font sizes, from largest to smallest.")
+
+(defun my/shift-css-font-size-up ()
+  "Shift CSS font-size to be larger."
+  (interactive)
+  (save-excursion
+    (cl-mapcar #'(lambda (size1 size)
+                   (goto-char (point-min))
+                   (let ((regexp (concat "\\(font-size *: *\\)" size))
+                         (newtext (concat "\\1" size1)))
+                     (while (re-search-forward regexp nil t)
+                       (replace-match newtext))))
+               my/css-font-sizes (cdr my/css-font-sizes))))
 
 (provide 'llasram-misc)
 
